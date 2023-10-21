f50d1fcd2cec7ed2d6b34e6dd8eb6ab0fa80b413
Misael-GC <misael.gomez1623@gmail.com>
2023-10-20 18:57:02 -0600

    alarma, pendiente de mejorar - bugs

--- alexa.py
+++ alexa.py
@@ -1,21 +1,24 @@
 import speech_recognition as sr
-import pyttsx3, pywhatkit, wikipedia, datetime, keyboard 
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard
 from pygame import mixer
 # import pyautogui
 
+# Definición del nombre del asistente
 name = "Alexa"
-listener = sr.Recognizer()
+listener = sr.Recognizer() # Inicialización del reconocedor de voz
 engine = pyttsx3.init()
 
+# Obtención de las voces disponibles y selección de la primera voz
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
 
+# Función para que el asistente hable
 def talk(text):
     engine.say(text)
     engine.runAndWait()
     
-
+# Función para escuchar y reconocer el habl
 def listen():
     try:
         with sr.Microphone() as source:
@@ -29,6 +32,7 @@ def listen():
         pass
     return rec
 
+# Función principal del asistente
 def run_alexa():
     while True:
         rec = listen()
@@ -36,7 +40,7 @@ def run_alexa():
             music = rec.replace('reproduce', '')
             print("Reproduciendo " + music)
             talk("Reproduciendo " + music)
-            pywhatkit.playonyt(music)
+            pywhatkit.playonyt(music) # Busca y reproduce la canción en YouTube
             # pyautogui.press('space')
         elif 'busca' in rec or 'wikipedia' in rec:
             search = rec.replace('busca', '')
@@ -44,6 +48,21 @@ def run_alexa():
             wiki = wikipedia.summary(search, 1)
             print(search + ": " + wiki)
             talk(wiki)
+        elif 'alarma' in rec:
+            num = rec.replace('alarma', '')
+            num = num.strip()
+            talk("Alarma activada a las " + num + " horas")
+            while True:
+                if datetime.datetime.now().strftime('%H:%M') == num:
+                    print("DESPIERTA!!!")
+                    talk("¿Es hora de despertar!")
+                    mixer.init()
+                    mixer.music.load("auronplay-alarma.mp3")
+                    mixer.music.play()
+                    if keyboard.read_key() == "s":
+                        mixer.music.stop()
+                    break
+                    
 
 if __name__ == '__main__':
     run_alexa()
